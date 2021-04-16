0e51554efb1baacdd6c8da7b0ea9e790f733c0de
enemyofsoul <82306182+enemyofsoul@users.noreply.github.com>
2021-04-16 11:57:28 +0530

    Update dice roll simulator.py

--- dice roll simulator.py
+++ dice roll simulator.py
@@ -1,5 +1,6 @@
 import random
 l = [0,1,2,3,4,5,6]
+h = [0,1,2,3,4,5]
 n = random.choice(l)
 print(n)
 a = 0
@@ -8,3 +9,7 @@ while(a==0):
     if(b=="yes" or b =="YES" or b =="Yes"):
         n = random.choice(l)
         print(n)
+        if(n==6):
+            n = random.choice(h)
+            print(n)
+            
